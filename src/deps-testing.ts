7ac1a87c793464289cbee930995150841d235007
Jeff Mendez <jeffmendez19@gmail.com>
2021-07-10 12:37:24 -0400

    add version for bench

--- src/deps-testing.ts
+++ src/deps-testing.ts
@@ -1,10 +1,13 @@
 export {
   prettyBenchmarkResult,
   prettyBenchmarkDown,
-  prettyBenchmarkProgress
-} from "https://deno.land/x/pretty_benching@v0.3.3/mod.ts"
-export { runBenchmarks, bench } from "https://deno.land/std/testing/bench.ts"
+  prettyBenchmarkProgress,
+} from "https://deno.land/x/pretty_benching@v0.3.3/mod.ts";
+export {
+  runBenchmarks,
+  bench,
+} from "https://deno.land/std@0.93.0/testing/bench.ts";
 export {
   assertEquals,
-  assertMatch
-} from "https://deno.land/std@0.93.0/testing/asserts.ts"
+  assertMatch,
+} from "https://deno.land/std@0.93.0/testing/asserts.ts";
